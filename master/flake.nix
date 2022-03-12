8cf058b3db897440c8b4f5136c298162237fe3c1
hugosenari <hugosenari@gmail.com>
2022-03-12 04:30:40 -0300

    chore: re index nimpmda

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''PCP PMDA module bindings'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimpmda-master.flake = false;
+  inputs.src-nimpmda-master.owner = "jasonk000";
+  inputs.src-nimpmda-master.ref   = "refs/heads/master";
+  inputs.src-nimpmda-master.repo  = "nimpmda";
+  inputs.src-nimpmda-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimpmda-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimpmda-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
